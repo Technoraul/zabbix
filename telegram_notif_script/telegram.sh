687e2ab9237324eb2a08b53b47cd2e126cefc159
Technoraul <66273158+Technoraul@users.noreply.github.com>
2020-06-16 13:35:24 +0300

    Update telegram.sh

--- telegram_notif_script/telegram.sh
+++ telegram_notif_script/telegram.sh
@@ -6,3 +6,7 @@ subj="$2"
 message="$3"
 
 /usr/bin/curl -s --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${chat}\",\"text\":\"${subj}\n${message}\"}" "https://api.tlgr.org/bot${token}/sendMessage"
+
+
+
+### Place here token='{token}' and here bot${token} ID of your bot
